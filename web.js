946234ef840658a0594c4b9185149c8a19f0b6e5
Rodrigo Vianna <rvianna@globo.com>
2013-07-09 02:34:59 +0000

    web.js reading index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var buf = fs.readFileSync('index.html');
   var str = buf.toString();  
-  response.send(str);
+  //response.send(str);
 });
 
 var port = process.env.PORT || 5000;
