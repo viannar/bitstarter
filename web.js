dcf55f0b2b8afde68fae69679e9e7c5ded58fadc
Rodrigo Vianna <rvianna@globo.com>
2013-07-09 02:22:49 +0000

    Teste

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf = new Buffer(fs.readfile(index.html));
+  var str = buf.toString();  
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
